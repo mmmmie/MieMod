6e1b143accad13da1ce7ef031dbc2812ac3dc39f
Kewei Zhang <4mmmmie@gmail.com>
2026-03-19 13:40:06 +0000

    Improve build pipeline

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+dotnet build -c Release MieMod.csproj
+pkill -SIGTERM -f 'Slay the Spire 2' || true
+pkill -SIGKILL -f 'Slay the Spire 2' || true
+cp -r ~/Library/Application\ Support/Steam/steamapps/common/Slay\ the\ Spire\ 2/SlayTheSpire2.app/Contents/MacOS/mods/ ~/Desktop/SlayTheSpire2_copy.app/Contents/MacOS/mods/
+open -n ~/Desktop/SlayTheSpire2_copy.app --args --remote-debug tcp://127.0.0.1:6007
